9270bd9c8353ecb1d5dc88647bf6d1050c88e432
James Devine <james.devine@microsoft.com>
2022-07-19 09:29:14 +0100

    clean debug

--- dmesg.c
+++ dmesg.c
@@ -10,20 +10,10 @@ struct CodalLogStore codalLogStore;
 static void logwriten(const char *msg, int l) {
     uint32_t pri = NVIC_BlockIRQs();
     if (codalLogStore.ptr + l >= sizeof(codalLogStore.buffer)) {
-#if 1
         codalLogStore.buffer[0] = '.';
         codalLogStore.buffer[1] = '.';
         codalLogStore.buffer[2] = '.';
         codalLogStore.ptr = 3;
-#else
-        // this messes with timings too much
-        const int jump = sizeof(codalLogStore.buffer) / 4;
-        codalLogStore.ptr -= jump;
-        memmove(codalLogStore.buffer, codalLogStore.buffer + jump, codalLogStore.ptr);
-        // zero-out the rest so it looks OK in the debugger
-        memset(codalLogStore.buffer + codalLogStore.ptr, 0,
-               sizeof(codalLogStore.buffer) - codalLogStore.ptr);
-#endif
     }
     if (l + codalLogStore.ptr >= sizeof(codalLogStore.buffer))
         return; // shouldn't happen
@@ -172,15 +162,9 @@ int cust_vsprintf(char *dst, unsigned dstsize, const char *format, va_list ap) {
 
 
 void codal_vdmesg(const char *format, va_list ap) {
-    // if (format[0] == '!')
-    //    pin_pulse(PIN_X0, 1);
     char tmp[80];
     cust_vsprintf(tmp, sizeof(tmp) - 1, format, ap);
     int len = strlen(tmp);
-#if JD_LORA
-    while (len && (tmp[len - 1] == '\r' || tmp[len - 1] == '\n'))
-        len--;
-#endif
     tmp[len] = '\n';
     tmp[len + 1] = 0;
     logwriten(tmp, len + 1);
